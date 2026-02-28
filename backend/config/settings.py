1072998ea0830df5b6e3ee928fb5179ea62333d3
Kalidas Rahul <kalidasrahul12@gmail.com>
2026-02-28 18:03:41 +0530

    Production setup: Supabase DB + Render deployment config

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m@yv#bx$sjzjy#7n*&wrf1jocc1-4f^78nc1lp7-3j!9moje6%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -78,10 +78,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        "postgresql://postgres:Kalidas@2606!@db.lagmbkkfqptulxckwnpr.supabase.co:5432/postgres",
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
